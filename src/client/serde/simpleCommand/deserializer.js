ef55d0df63755a45755711c583fba6e44c8ebb46
danielsinai <daniel.sinai.main@gmail.com>
2021-07-25 20:44:44 +0300

    deserialze payload and simple command

--- src/client/serde/simpleCommand/deserializer.js
+++ src/client/serde/simpleCommand/deserializer.js
@@ -1,10 +1,6 @@
 const pulsarApi = require('../../../commands/protocol/pulsar/pulsar_pb');
 
 const deserializer = (buffer) => {
-  // Currently un used, probably will find usage later on
-  const totalSize = buffer.readInt32BE();
-  const commandSize = buffer.readInt32BE(4);
-
   const deserializedBaseCommand = pulsarApi.BaseCommand.deserializeBinary(buffer.slice(8));
   const baseCommandObject = deserializedBaseCommand.toObject();
   const typeNumber = deserializedBaseCommand.getType();
--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const client = require('./client');
   const configuredClient = client({
     broker: '192.168.99.100:6650',
     timeout: 100,
-    jwt: 'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJhZG1pbiJ9.Bs8_6JvaiKF0WLPpTNMgCgC1KJPvKqdzsGmB7GSfErs',
+    jwt: 'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJhZG1pbiJ9.uLA81lDfy7qpGJ2Bu6cJcTEvISPGlTsyxdiCqgewuYs',
   });
   const { responseEmitter, sendSimpleCommandRequest } = configuredClient.connect();
 })();
