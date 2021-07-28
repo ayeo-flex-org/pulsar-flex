86eb13743329b66d17570f64c291bfc458aa6363
danielsinaimain <daniel.sinai.main@gmail.com>
2021-07-28 21:58:23 +0300

    added tcp chunking

--- src/client/resolver/data.js
+++ src/client/resolver/data.js
@@ -8,17 +8,30 @@ const isSimpleCommand = (buffer) => {
   return totalSize - commandSize === 4;
 };
 
+let latestChunk = null;
+
 const data = (buffer) => {
   let currentBufferIndex = 0;
+  buffer = latestChunk ? Buffer.concat([latestChunk, buffer]) : buffer;
   while (currentBufferIndex < buffer.length) {
+    if (buffer.length - currentBufferIndex < 4) {
+      latestChunk = buffer.slice(currentBufferIndex, buffer.length);
+      return;
+    }
     const bufferSize = buffer.readInt32BE(currentBufferIndex) + 4;
+
+    if (bufferSize + currentBufferIndex > buffer.length) {
+      latestChunk = buffer.slice(currentBufferIndex, buffer.length);
+      return;
+    }
+
+    const slicedBuffer = buffer.slice(currentBufferIndex, currentBufferIndex + bufferSize);
+
     if (isSimpleCommand(buffer)) {
-      const { type, command } = serde.simpleCommand.deserializer(buffer);
+      const { type, command } = serde.simpleCommand.deserializer(slicedBuffer);
       emitter.data.emit(type, { command });
     } else {
-      const { type, command, payload, metadata } = serde.payloadCommand.deserializer(
-        buffer.slice(currentBufferIndex, currentBufferIndex + bufferSize)
-      );
+      const { type, command, payload, metadata } = serde.payloadCommand.deserializer(slicedBuffer);
       emitter.data.emit(type, { command, metadata, payload });
     }
     currentBufferIndex += bufferSize;
