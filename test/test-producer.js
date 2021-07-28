9a8200d1671965181f9aac9e0df11c57bd5056e8
galrose <bengal.rosenberg@gmail.com>
2021-07-28 12:57:48 +0300

    simple message send works

--- test/test-producer.js
+++ test/test-producer.js
@@ -2,17 +2,22 @@ const { Pulsar, Producer } = require('../src');
 
 (async () => {
   const pulsar = new Pulsar({
-    broker: '192.168.99.100:6650',
+    broker: '20.101.147.31:6650',
     timeout: 1000,
   });
 
+  console.log('pulsar');
   const producer = new Producer({
     pulsar,
     topic: 'public/default/galrose',
     producerConfiguration: {},
   });
 
+  console.log('producer');
   await producer.create();
+  console.log('created');
   await producer.send({ payload: 'bla' });
+  console.log('sent');
   await producer.close();
+  console.log('close');
 })();
