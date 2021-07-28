38b3963929d8d36f63e5f499709bece49a70b1a7
ronfarkash <ronfarkashxxo@gmail.com>
2021-07-28 10:09:11 +0300

    added interact script, to enter pulsar container

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 const Pulsar = require('./client');
 const producer = require('./producer');
+const consumer = require('./consumer');
 
 const pulsar = {
   Pulsar,
   producer,
+  consumer,
 };
 
 module.exports = pulsar;
--- test/test-client.js
+++ test/test-client.js
@@ -2,7 +2,7 @@ const { Pulsar } = require('../src');
 
 (async () => {
   const pulsar = new Pulsar({
-    broker: '192.168.99.100:6650',
+    broker: 'localhost:6650',
     timeout: 1000,
   });
 
