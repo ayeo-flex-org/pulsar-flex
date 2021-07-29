875b57883c42f9b50c6ecf6f02b37e6f52d6a252
galrose <bengal.rosenberg@gmail.com>
2021-07-29 17:39:03 +0300

    close the connection before trying to reconnect

--- src/producer/services/reconnect.js
+++ src/producer/services/reconnect.js
@@ -2,6 +2,7 @@ const reconnect = (client, create, setConnected, { reconnectTimeout = 1000 }) =>
   const responseEvents = client.getResponseEvents();
   responseEvents.on('closeProducer', () => {
     setConnected(false);
+    client.getCnx().close();
     setTimeout(function () {
       create();
     }, reconnectTimeout);
