4ad7f46deaecda677d37eacd2b39854e09599a62
ronfarkash <ronfarkashxxo@gmail.com>
2021-08-01 14:50:18 +0100

    started ack tests, made test utils more accessible

--- test/consumer/index-spec.js
+++ test/consumer/index-spec.js
@@ -1,31 +1,35 @@
 const { Pulsar, Consumer } = require('../../src');
 const config = require('../config');
 const assert = require('assert');
-const asyncExec = require('../utils/asyncExec');
+const utils = require('../utils');
 
 const { jwt, discoveryServers, topic , containerName} = config;
 
-describe('Consumer test', function () {
+describe('Consumer tests', function () {
+  afterEach(() => {
+    console.log('Clearing Backlog...');
+    utils.clearBacklog();
+  })
+  const cons = new Consumer({
+    discoveryServers,
+    jwt,
+    topic: 'persistent://public/default/test',
+    subscription: 'subscription',
+    subType: Consumer.SUB_TYPES.EXCLUSIVE,
+    consumerName: 'Consy',
+    readCompacted: false,
+    receiveQueueSize: 1000,
+  })
   describe('Consume Messages', function () {
     it('should not throw exception', async function () {
       this.timeout(30000);
-      const cons = new Consumer({
-        discoveryServers,
-        jwt,
-        topic: 'persistent://public/default/test',
-        subscription: 'subscription',
-        subType: Consumer.SUB_TYPES.EXCLUSIVE,
-        consumerName: 'Consy',
-        readCompacted: false,
-        receiveQueueSize: 1,
-    })
       try {
         await cons.subscribe();
         let expectedMessages = ['hello', 'world', 'goodbye'];
         let messages = [];
-
-        await asyncExec(`docker exec ${containerName} /pulsar/bin/pulsar-client produce -m ${expectedMessages.join(',')} ${topic}`)
-
+        
+        utils.produceMsgs(expectedMessages);
+        
         await new Promise((resolve, reject) => {
             cons.run({
                 onMessage: ({ ack, message, data }) => {
@@ -46,6 +50,12 @@ describe('Consumer test', function () {
         console.log(e);
         assert.ok(false);
       }
+    });
+    describe('Manual Ack', function() {
+      it('Should not re-read the message', async function() {
+        await cons.subscribe();
+
+      });
     });
   });
 });
---
+++ test/utils/index.js
@@ -0,0 +1,11 @@
+const asyncExec = require('./asyncExec');
+const clearBacklog = require('./clearBacklog');
+const produceMsgs = require('./produceMsgs');
+
+const utils = {
+  asyncExec,
+  clearBacklog,
+  produceMsgs,
+};
+
+module.exports = utils;
