4c925b1cbfbfd14efd807e3c77a87a09c4fed91e
galrose <bengal.rosenberg@gmail.com>
2021-08-18 10:43:30 +0300

    removed prioritize check in redeliver

--- src/commands/redeliverUnacknowledgedMessages.js
+++ src/commands/redeliverUnacknowledgedMessages.js
@@ -12,15 +12,11 @@ const redeliverUnacknowledgedMessages = ({
   const messageIds = Array.isArray(messageIdData)
     ? messageIdData.map((data) => common.convertToMessageIdData(data))
     : [common.convertToMessageIdData(messageIdData)];
-  return prioritizeUnacknowledgedMessages
-    ? baseCommand.setRedeliverunacknowledgedmessages(
-        new pulsarApi.CommandRedeliverUnacknowledgedMessages().setConsumerId(consumerId)
-      )
-    : baseCommand.setRedeliverunacknowledgedmessages(
-        new pulsarApi.CommandRedeliverUnacknowledgedMessages()
-          .setMessageIdsList(messageIds)
-          .setConsumerId(consumerId)
-      );
+  return baseCommand.setRedeliverunacknowledgedmessages(
+    new pulsarApi.CommandRedeliverUnacknowledgedMessages()
+      .setMessageIdsList(messageIds)
+      .setConsumerId(consumerId)
+  );
 };
 
 module.exports = redeliverUnacknowledgedMessages;
